5c856accf5737aac34ba66612d2818c7d04fef0d
zakirovra <zakirovra@gmail.com>
2022-12-15 16:25:22 +0400

    add 3 tasks with List Comprehension

---
+++ hw6/task01.py
@@ -0,0 +1,24 @@
+"""
+Задайте список из n чисел последовательности (1 + 1/n)^n.
+Вывести в консоль сам список и сумму его элементов.
+"""
+
+
+def make_list(num):
+    # res_list = []
+    # for i in range(num):
+    #     res_list.append(round((1 + 1 / (i + 1) ** (i + 1)), 4))
+
+    # List Comprehension
+    res_list = [(round((1 + 1 / (i + 1) ** (i + 1)), 4)) for i in range(num)]
+    return res_list
+
+
+if __name__ == '__main__':
+    try:
+        count = int(input('Введите число: '))
+        result_list = make_list(count)
+        print(f'Список: {result_list}')
+        print(f'Сумма элементов: {sum(result_list)}')
+    except (ValueError, Exception) as err:
+        print('Ошибка!', err)
